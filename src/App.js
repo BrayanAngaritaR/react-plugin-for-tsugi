7dd190069823d32f083902f8cb990cc90b82543f
Brayan Angarita <brayanangarita11@gmail.com>
2021-07-29 09:39:36 -0500

    Homepage changed to .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import {Table, Button, Modal, ModalBody, ModalHeader, FormGroup, Container, ModalFooter} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit the file <code>src/App.js</code> to continue.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const data = [
+   {id: 1, personaje: "Name", description: "Same"},
+   {id: 2, personaje: "Name2", description: "Same2"},
+];
+
+class App extends Component{
+
+   state = {
+      code: ""
+   };
+
+   handleAdd = async e => {
+      await this.setState({
+         text: e.target.value
+      })
+   }
+
+   handleSubmit = e => {
+      e.preventDefault();
+      console.log(this.state.text);
+
+      let formData = new FormData();
+
+      formData.append("code", this.state.code);
+      const url = "http://159.65.219.167/mod/react-attend/api/attend.php";
+
+      axios.post(url, formData)
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+   }
+
+   render(){
+      return(
+      <>
+      <Container>
+      <FormGroup>
+      <label>Enter a code:</label>
+      <input 
+         onChange={this.handleAdd}
+         className="form-control mt-2 mb-2" 
+         placeholder="Enter the code" 
+         name="code"
+         type="number" 
+         />
+      </FormGroup>
+      <Button id="checkCode" onClick={this.handleSubmit} color="primary">Check code</Button>
+      </Container>
+      </>)
+   }
 }
 
 export default App;
